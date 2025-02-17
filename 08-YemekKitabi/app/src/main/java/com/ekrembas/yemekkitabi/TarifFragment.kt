336c9e22fb82d8c6f1069294f7d42b615a24f3c4
Ekrem Bas <ekrembas03@outlook.com>
2025-02-17 17:05:50 +0300

    07.04-Izin kontrolleri

--- 08-YemekKitabi/app/src/main/java/com/ekrembas/yemekkitabi/TarifFragment.kt
+++ 08-YemekKitabi/app/src/main/java/com/ekrembas/yemekkitabi/TarifFragment.kt
@@ -1,11 +1,16 @@
 package com.ekrembas.yemekkitabi
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.ekrembas.yemekkitabi.databinding.FragmentTarifBinding
+import com.google.android.material.snackbar.Snackbar
 
 class TarifFragment : Fragment() {
 
@@ -56,7 +61,31 @@ class TarifFragment : Fragment() {
     }
 
     fun gorselSec(view: View) {
-
+        // izin verilmemisse izni istememiz lazim
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                // snackbar gosterilmesi lazim, kullanicidan neden izin istedigimizi bir kez soylememiz lazim
+                Snackbar.make(
+                    view,
+                    "Galeriye erişip görsel seçmeniz gerekmektedir!",
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction("İzin Ver", View.OnClickListener {
+                    // izin isteyecegiz
+                }).show()
+            } else {
+                // izin isteyecegiz
+            }
+        } else {
+            // izin onceden verilmis, galeriye gidilebilir
+        }
     }
 
     override fun onDestroyView() {
